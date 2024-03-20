c375a70c03130921d55fd6e44a6fd85c5e4a7035
matheus mozart <mmsnborges@gmail.com>
2024-03-20 13:08:36 -0400

    test de threads

--- src/br/com/alura/AppBanco.java
+++ src/br/com/alura/AppBanco.java
@@ -6,8 +6,9 @@ public class AppBanco {
 
     public static void main(String[] args) {
         Cliente cliente = new Cliente("João");
-        Conta conta = new Conta(cliente, new BigDecimal("150"));
+            Conta conta = new Conta(cliente, new BigDecimal("150"));
         var operacao = new OperacaoSaque(conta, new BigDecimal("150"));
+
 
 
 
